eabd76acecc1e94bff05b17dd4728c1d67f6ca71
Jos3MA56 <20230087@uthh.edu.mx>
2025-11-01 11:32:31 -0600

    modified:   src/email/transporter.js

--- src/email/transporter.js
+++ src/email/transporter.js
@@ -1,15 +1,10 @@
 import nodemailer from "nodemailer";
-
 export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST || "smtp.gmail.com",
   port: Number(process.env.SMTP_PORT || 465),
   secure: true,
   auth: { user: process.env.SMTP_USER, pass: process.env.SMTP_PASS },
-  pool: true,
-  maxConnections: 3,
-  maxMessages: 50,
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
+  pool: true, maxConnections: 3, maxMessages: 50,
+  connectionTimeout: 10000, greetingTimeout: 10000, socketTimeout: 10000,
   tls: { minVersion: "TLSv1.2" },
 });
