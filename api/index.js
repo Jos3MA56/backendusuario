e1fcc14479c58980cee420248bd83585c0cc1901
Jos3MA56 <20230087@uthh.edu.mx>
2025-11-02 20:44:45 -0600

    new file:   api/[...path].js 	new file:   api/index.js 	new file:   api/ping.js

---
+++ api/index.js
@@ -0,0 +1,6 @@
+// api/index.js
+import serverless from "serverless-http";
+import app from "../src/app.js";
+
+export const config = { api: { bodyParser: false } };
+export default serverless(app);
