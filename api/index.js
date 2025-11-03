5e04b26bc55d3cfcdc69af788e279040c12f1f33
Jos3MA56 <20230087@uthh.edu.mx>
2025-11-03 14:10:14 -0600

    new file:   api/[...path].js 	new file:   api/index.js 	new file:   api/ping.js 	deleted:    src/api/[...path].js 	deleted:    src/api/index.js 	deleted:    src/api/ping.js

---
+++ api/index.js
@@ -0,0 +1,19 @@
+// api/index.js
+import "dotenv/config";
+import mongoose from "mongoose";
+import app from "../src/app.js";
+import serverless from "serverless-http";
+
+let conn;
+async function connectOnce() {
+    if (!conn) {
+        conn = mongoose.connect(process.env.MONGO_URI, {});
+    }
+    await conn;
+}
+
+export default async function handler(req, res) {
+    await connectOnce();
+    const h = serverless(app);
+    return h(req, res);
+}
