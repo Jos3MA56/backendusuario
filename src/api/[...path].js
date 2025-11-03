aa9dbbcaddd5ec1634f70d0901fb615e601adb49
Jos3MA56 <20230087@uthh.edu.mx>
2025-11-02 20:45:28 -0600

    Delete src/api directory

--- src/api/[...path].js
+++
@@ -1,5 +0,0 @@
-import serverless from "serverless-http";
-import app from "../app.js";   // ojo: ../app.js porque estás en src/api
-
-export const config = { api: { bodyParser: false } };
-export default serverless(app);
--- src/api/index.js
+++
@@ -1,6 +0,0 @@
-// api/index.js
-import serverless from "serverless-http";
-import app from "../src/app.js";
-
-export const config = { api: { bodyParser: false } };
-export default serverless(app);
