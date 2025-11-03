679a97983cc3cd42c22c0488806b996ac2e3637c
Jos3MA56 <20230087@uthh.edu.mx>
2025-11-03 16:22:10 -0600

    modified:   api/index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import mongoose from 'mongoose';
-import app from '../src/app.js';
 import serverless from 'serverless-http';
+import app from '../src/app.js';
 
 let conn;
 async function connectOnce() {
@@ -12,7 +12,13 @@ async function connectOnce() {
 }
 
 export default async function handler(req, res) {
-    await connectOnce();
-    const h = serverless(app);
-    return h(req, res);
+    try {
+        await connectOnce();
+        const h = serverless(app);
+        return h(req, res);
+    } catch (e) {
+        console.error('Serverless error:', e);
+        res.statusCode = 500;
+        res.end('Internal error');
+    }
 }
