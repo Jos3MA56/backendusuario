d7ec503f6b34c35c90af372360cb541c594df0b7
Jos3MA56 <20230087@uthh.edu.mx>
2025-12-02 00:37:22 -0600

    Create PasswordReset.js

---
+++ src/models/PasswordReset.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const passwordResetSchema = new mongoose.Schema(
+  {
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, index: true },
+    tokenHash: { type: String, required: true },
+    expiresAt: { type: Date, required: true, index: true },
+    usedAt: { type: Date, default: null }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("PasswordReset", passwordResetSchema);
